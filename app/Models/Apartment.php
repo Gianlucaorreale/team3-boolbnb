b11332360dd9f6d6db574278bab5fcb0899df50f
giomen88 <giorgia9288@virgilio.it>
2022-10-19 10:59:05 +0200

    relationship models added

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Apartment extends Model
 {
-    //
+    protected $fillable = ['descriptive_title', 'num_rooms', 'num_bathroom', 'num_bed', 'visibility', 'address', 'latitude', 'longitude', 'image'];
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    public function services() {
+        return $this->belongsToMany('App\Models\Service');
+    }
+
+    public function sponsors() {
+        return $this->belongsToMany('App\Models\Sponsor');
+    }
+
+    public function messages() {
+        return $this->hasMany('App\Models\Message');
+    }
+
+    public function views() {
+        return $this->hasMany('App\Models\View');
+    }
+
+
 }
