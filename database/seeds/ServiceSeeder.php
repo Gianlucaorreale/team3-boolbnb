df7f07bc148ca5456666cea9107aaf085512e4f8
giomen88 <giorgia9288@virgilio.it>
2022-10-19 12:46:41 +0200

    service seeder

--- database/seeds/ServiceSeeder.php
+++ database/seeds/ServiceSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Service;
 
 class ServiceSeeder extends Seeder
 {
@@ -11,6 +12,18 @@ class ServiceSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $services = ['Piscina', 'Portineria', 'Sauna', 'WiFi', 'Posto Macchina', 
+        'Vista Mare', 'Terrazza', 'Giardino', 'Spiaggia privata', 'Biancheria', 
+        'Smart Assistance', 'Camera fumatori'];
+
+        
+        forEach($services as $service) {
+        $new_service = new Service();
+
+        $new_service->label = $service;
+        
+        $new_service->save();
+        };
+
     }
 }
