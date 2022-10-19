c20839e9db150b194d1d69323af906b5aa47c4e2
Fernweh99 <daniele.dimento@gmail.com>
2022-10-19 15:53:00 +0200

    scaffolding login authentication

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::middleware('auth')->prefix('admin')->namespace('Admin')->name('admin.')->group(function (){
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('apartments', 'ApartmentController');
+    Route::get('/{any}', function () {
+        abort('404');
+    })->where('any', '.*');
+});
