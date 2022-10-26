cbd507db5c7b477aed244107d602e6b2dfd4b3ee
Fernweh99 <daniele.dimento@gmail.com>
2022-10-26 15:21:37 +0200

    implement map on vue

--- resources/js/components/apartments/SearchBarApartment.vue
+++ resources/js/components/apartments/SearchBarApartment.vue
@@ -4,65 +4,65 @@
         type="text"
         class="me-2 rounded border-0 p-2"
     />
-      <button class="rounded border-0 py-2 px-4" onclick="search()">
+      <button class="rounded border-0 py-2 px-4" @click="search">
         Cerca
       </button>
       <div id="mymap" style="height:50vh;">
-        {{map}}
     </div>
     </div>
   </template>
-  
+
   <script>
   export default {
     name: "SearchBarApartment",
     data() {
       return {
         searchText: "",
-         
+        Rome: [41.5330, 12.3040],
       };
     },
   
     methods: {
-       
-        APIKEY: "xnU4V8EAyAUoA2mApWozIuuMusi5WDmF",
-         Rome: [41.5330, 12.3040],
-         map: tt.map({
-           key:APIKEY,
-           container:'mymap',
-           center: Rome,
-           style:'tomtom://vector/1/basic-main'
-        }),
-
-       moveMap (lnglat){
+      moveMap (lnglat){
+        const map = tt.map({
+          key:"k8P3Rx9lwVUMwJiJA3JF9ARIMpojuobA",
+          center: this.Rome,
+          container: 'mymap',
+          style:'tomtom://vector/1/basic-main',
+          });
         map.flyTo({
             center: lnglat,
-            zoom: 20,
+            zoom: 13,
         })
        }, 
 
        handleResults (result){
         if(result.results){
-           moveMap(result.results[0].position)
+           this.moveMap(result.results[0].position)
         }
        }, 
 
         search () {
-        tt.services.fuzzySearch({
-            key:APIKEY,
-            query : document.getElementById("query").value,
-        }).go().then(handleResults)
-       } 
-     }
+          tt.services.fuzzySearch({
+              key:"k8P3Rx9lwVUMwJiJA3JF9ARIMpojuobA",
+              query : document.getElementById("query").value,
+          }).go().then(this.handleResults)
+       },
+
+       getMap () {
+         const map = tt.map({
+          key:"k8P3Rx9lwVUMwJiJA3JF9ARIMpojuobA",
+          center: this.Rome,
+          container: 'mymap',
+          style:'tomtom://vector/1/basic-main',
+          });
+       }
+      },
+      mounted(){
+        this.getMap();
+      }
    }
   </script>
   
   <style lang="scss" scoped>
   </style>
-
-  <script>
-   
-   var APIKEY = "xnU4V8EAyAUoA2mApWozIuuMusi5WDmF"
-        var Rome = [41.5330, 12.3040]
-       
-</script>
