8cb9471435eea9ed95cd58b1beca40ae05f6c4bd
giomen88 <giorgia9288@virgilio.it>
2022-10-27 11:18:03 +0200

    modified search bar apartment

--- resources/js/components/apartments/SearchBarApartment.vue
+++ resources/js/components/apartments/SearchBarApartment.vue
@@ -4,8 +4,20 @@
       <div class="row">
         <div class="col-12">
           <div class="input-group">
-            <input type="text" class="form-control" :class="{ 'is-invalid': searchError }" placeholder="Location" v-model="dist" @keyup.enter="searchApartments"/>
-            <button class="btn btn-outline-secondary" type="button" id="search" @click="searchApartments">
+            <input
+              type="text"
+              class="form-control"
+              :class="{ 'is-invalid': searchError }"
+              placeholder="Location"
+              v-model="dist"
+              @keyup.enter="searchApartments"
+            />
+            <button
+              class="btn btn-outline-secondary"
+              type="button"
+              id="search"
+              @click="searchApartments"
+            >
               <i class="fa-solid fa-magnifying-glass"></i> Cerca
             </button>
           </div>
@@ -13,20 +25,35 @@
             <label for="customRange1" class="form-label fs-4">
               Seleziona il raggio
             </label>
-            <input v-model="radius" @change="getApartments" min="5" max="50" step="5" type="range" class="form-range" id="customRange1" :disabled="!dist"/>
+            <input
+              v-model="radius"
+              @change="getApartments"
+              min="0"
+              max="50"
+              step="1"
+              type="range"
+              class="form-range"
+              id="customRange1"
+              :disabled="!dist"
+            />
             <p v-show="dist" class="m-0">Raggio attuale: {{ radius }} Km</p>
           </div>
           <div class="col-8 offset-2">
             <div id="map"></div>
           </div>
-          <TheLoader v-if="isLoading"/>
-          <section v-else id="detail-apartment" class="container-fluid my-5 p-5">
+          <TheLoader v-if="isLoading" />
+          <section
+            v-else
+            id="detail-apartment"
+            class="container-fluid my-5 p-5"
+          >
             <h1 class="text-center" v-if="!apartments.length">
               La Ricerca non ha prodotto risultati!
             </h1>
 
             <div v-else class="container-card">
-              <CardApartment class="card-apartment"
+              <CardApartment
+                class="card-apartment"
                 v-for="apartment in apartments"
                 :key="apartment.id"
                 :apartment="apartment"
@@ -40,8 +67,8 @@
 </template>
 
 <script>
-import TheLoader from '../generics/TheLoader.vue';
-import CardApartment from './CardApartment.vue';
+import TheLoader from "../generics/TheLoader.vue";
+import CardApartment from "./CardApartment.vue";
 export default {
   components: { CardApartment, TheLoader },
   name: "SearchBarApartment",
@@ -59,19 +86,21 @@ export default {
     };
   },
   methods: {
-    fetchApartments(){
+    fetchApartments() {
       this.isLoading = true;
-      axios.get('http://127.0.0.1:8000/api/apartments').then(res=>{
-        this.apartments = res.data;
-      })
-      .catch((err) => {
-        console.error(err);
-      })
-      .then(() => {
-        console.log("chiamata terminata Appartamenti");
-        this.isLoading = false;
-        this.getMap();
-      });
+      axios
+        .get("http://127.0.0.1:8000/api/apartments")
+        .then((res) => {
+          this.apartments = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+        .then(() => {
+          console.log("chiamata terminata Appartamenti");
+          this.isLoading = false;
+          this.getMap();
+        });
     },
     searchApartments() {
       this.searchError = false;
@@ -80,13 +109,13 @@ export default {
         const config = {
           params: {
             countryCode: "IT",
-            municipality: this.dist,
+            query: this.dist,
             key: "k8P3Rx9lwVUMwJiJA3JF9ARIMpojuobA",
           },
         };
         delete axios.defaults.headers.common["X-Requested-With"];
         axios
-          .get("https://api.tomtom.com/search/2/structuredGeocode.json", config)
+          .get("https://api.tomtom.com/search/2/search/.json", config)
           .then((res) => {
             this.lat = res.data.results[0].position.lat;
             this.long = res.data.results[0].position.lon;
@@ -113,16 +142,22 @@ export default {
       if (lat1 == lat2 && lon1 == lon2) {
         return 0;
       } else {
-        var radlat1 = Math.PI * lat1/180
-        var radlat2 = Math.PI * lat2/180
-        var theta = lon1-lon2
-        var radtheta = Math.PI * theta/180
-        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-        dist = Math.acos(dist)
-        dist = dist * 180/Math.PI
-        dist = dist * 60 * 1.1515
-        if (unit=="K") { dist = dist * 1.609344 }
-        if (unit=="N") { dist = dist * 0.8684 }
+        var radlat1 = (Math.PI * lat1) / 180;
+        var radlat2 = (Math.PI * lat2) / 180;
+        var theta = lon1 - lon2;
+        var radtheta = (Math.PI * theta) / 180;
+        var dist =
+          Math.sin(radlat1) * Math.sin(radlat2) +
+          Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        dist = Math.acos(dist);
+        dist = (dist * 180) / Math.PI;
+        dist = dist * 60 * 1.1515;
+        if (unit == "K") {
+          dist = dist * 1.609344;
+        }
+        if (unit == "N") {
+          dist = dist * 0.8684;
+        }
         return dist;
       }
     },
@@ -130,19 +165,27 @@ export default {
       // Mappa al caricamento
       this.apartments = [];
       this.isLoading = true;
-      axios.get("http://127.0.0.1:8000/api/apartments").then((res) => {
+      axios
+        .get("http://127.0.0.1:8000/api/apartments")
+        .then((res) => {
           const apartments = res.data;
           if (this.lat && this.long) {
             this.apartments = apartments.filter((apartment) => {
               const lat = apartment.latitude;
               const long = apartment.longitude;
-              if (this.distance(lat, long, this.lat, this.long, "K") < this.radius) {
+              if (
+                this.distance(lat, long, this.lat, this.long, "K") < this.radius
+              ) {
+                console.log(lat, long);
+                console.log(this.lat, this.long);
+                console.log(this.distance(lat, long, this.lat, this.long, "K"));
+
                 return true;
               }
               return false;
             });
           } else {
-            console.log('Nessun Appartamento nel raggio impostato')
+            console.log("Nessun Appartamento nel raggio impostato");
           }
         })
         .catch((err) => {
@@ -168,10 +211,7 @@ export default {
         map.on("load", () => {
           this.apartments.forEach((element) => {
             var marker = new tt.Marker()
-              .setLngLat([
-                element.longitude,
-                element.latitude,
-              ])
+              .setLngLat([element.longitude, element.latitude])
               .addTo(map);
             var popup = new tt.Popup({ offset: popupOffset }).setHTML(
               `<p class="mt-1">${element.descriptive_title}</p>
@@ -185,16 +225,13 @@ export default {
           key: "k8P3Rx9lwVUMwJiJA3JF9ARIMpojuobA",
           container: "map",
           center: [this.long, this.lat],
-          zoom: this.radius > 20 ? 8 : 10,
+          zoom: this.radius > 20 ? 15 : 20,
         });
         var popupOffset = 25;
         map.on("load", () => {
           this.apartments.forEach((element) => {
             var marker = new tt.Marker()
-              .setLngLat([
-                element.longitude,
-                element.latitude,
-              ])
+              .setLngLat([element.longitude, element.latitude])
               .addTo(map);
             var popup = new tt.Popup({ offset: popupOffset }).setHTML(
               `<p class="mt-1">${element.descriptive_title}</p>
@@ -217,12 +254,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.container-card{
+.container-card {
   display: flex;
   justify-content: space-between;
   align-items: stretch;
   flex-wrap: wrap;
-  .card-apartment{
+  .card-apartment {
     width: 320px;
     margin-bottom: 30px;
   }
